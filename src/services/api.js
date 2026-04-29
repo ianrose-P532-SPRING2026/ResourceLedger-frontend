cf20ef6f542fc12e95df25401252325d8e9e1634
Rose, Ian Christian <ianrose@iu.edu>
2026-04-29 09:38:00 -0400

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 export const host = axios.create({
-  baseURL: "http://localhost:8080/api",
-  //baseURL: "https://ianroseresourceledgerweek1.onrender.com",
+  //baseURL: "http://localhost:8080/api",
+  baseURL: "https://ianroseresourceledgerweek1.onrender.com",
   timeout: 30000,
 });
 
