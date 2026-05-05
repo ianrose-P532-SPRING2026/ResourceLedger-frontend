95806aa94820c3764da90aacb5e519c76c244fd0
Rose, Ian Christian <ianrose@iu.edu>
2026-05-04 23:25:30 -0400

    shiiiiiiiiiiiiit i forgot the localhost

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 export const host = axios.create({
-  baseURL: "http://localhost:8080/api",
-  //baseURL: "https://ianroseresourceledgerweek1.onrender.com/api",
+  //baseURL: "http://localhost:8080/api",
+  baseURL: "https://ianroseresourceledgerweek1.onrender.com/api",
   timeout: 30000,
 });
 
